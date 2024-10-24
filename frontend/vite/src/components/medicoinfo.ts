73e6398fff664eec187b5e8add4a477af6ab29e4
Bruno-Klisman <brunoserafim.dev@gmail.com>
2024-10-23 23:34:29 -0300

    Atualizando o frontend

---
+++ frontend/vite/src/components/medicoinfo.ts
@@ -0,0 +1,13 @@
+export interface PesquisaMedico {
+    id: number;
+    titulo: string;
+  }
+  const pesquisasMedicas: PesquisaMedico[] = [
+    { id: 1, titulo: "Mecanismos moleculares e genéticos no Alzheimer: Novas Terapias" },
+    { id: 2, titulo: "Estilo de Vida e Prevenção do Alzheimer: Dieta e Exercício" },
+    { id: 3, titulo: "Biomarcadores e Diagnóstico Precoce no Alzheimer" },
+    { id: 4, titulo: "Terapias Imunológicas para Alzheimer: Vacinas e Anticorpos" },
+    { id: 5, titulo: "Neuroinflamação e Sistema Imune no Alzheimer" },
+  ];
+  
+  export default pesquisasMedicas;
