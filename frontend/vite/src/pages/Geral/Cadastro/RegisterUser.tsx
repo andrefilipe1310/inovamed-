545db4dedbfe2bfa2b340b6f3b030fd07ac20b13
AyrtonF <ayrtonleonardo14@gmail.com>
2024-11-12 10:04:53 -0300

    iniciando conexão do frontend com o backend do representante do estudo na pagina de cadastro

--- frontend/vite/src/pages/Geral/Cadastro/RegisterUser.tsx
+++ frontend/vite/src/pages/Geral/Cadastro/RegisterUser.tsx
@@ -1,101 +1,170 @@
-import { useState } from "react"
-import { Link, useSearchParams } from "react-router-dom"
-import InputMask from 'react-input-mask';
+import { useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import InputMask from "react-input-mask";
+import api from "../../../config/axiosConfig";
 
-export default function RegisterUser(){
+export default function RegisterUser() {
+  const [searchParams] = useSearchParams();
+  const userType = searchParams.get("userType");
 
-    const [seachParams] = useSearchParams()
-    const userType = seachParams.get('userType')
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    id: null,
+    name: "",
+    email: "",
+    password: "",
+    roles: "REPRESENTATIVE",
+    phone: "",
+    clinicalRole: "",
+    experience: "",
+  });
 
-    const [showPassword, setShowPassword] = useState(false);
-
-    const togglePasswordVisibility = () => {
+  const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-    if (userType === "representative"){
-        
-      
-    return(
-        <div className="form-container">
-          <div className="logo-container"><img src="../../../../public/logo_branca.svg" alt="logo" /></div>
-          <h1 className="title-page" style={{marginTop:"0px"}}>CADASTRO</h1>
-          <div className="form-header">
-            <p>Representante Institucional</p>
-            <button className="change-user"><Link to={"/cadastro"} className="ajust-link">Trocar</Link></button>
-          </div>
-          <div className="form" >
-            <label htmlFor="nome">Nome completo <i>Obrigatório</i></label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              required
-            />
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
 
-            <label htmlFor="email">Email corporativo <i>Obrigatório</i></label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              required
-            />
-
-                <label htmlFor="instituicao">Instituição <i>Obrigatório</i></label>
-                <input
-                  type="text"
-                  id="institution"
-                  name="institution"
-                  required
-                />
-
-              <div className="phone-number">
-              <label htmlFor="contato">N° para contato <i>Obrigatório</i></label>
-                <InputMask
-                  mask="(99)99999-9999"
-                  required
-                  >
-                  {(inputProps) => <input {...inputProps} type="tel" />}
-                </InputMask>
-              </div>
-            
-
-            <label htmlFor="cargo">Cargo/função <i>Obrigatório</i></label>
-            <input
-              type="text"
-              id="function"
-              name="function"
-              required
-            />
-
-            <label htmlFor="experiencias">Experiências/qualificações <i>Obrigatório</i></label>
-            <textarea
-              id="exp"
-              name="exp"
-              rows={4}
-            />
-            <div className="ajust-section" >
-              <div>
-                <label htmlFor="senha">Criar senha <i>Obrigatório</i></label>
-                <input
-                  type={showPassword ? 'text' : 'password'}
-                  id="password"
-                  name="password"
-                  required
-                />
-              </div>
-              <button onClick={togglePasswordVisibility}>see</button>
-            </div>
-
-            <button type="submit" className="submit">FAZER CADASTRO</button>
-          </div>
-    </div>
-    )
-}
-    else{
-        return(
-            <div>AINDA EM PRODUÇÃO!!</div>
-        )
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Dados do formulario enviados:", formData);
+    try {
+      const response = await api.post("/clinical-representative", formData);
+      console.log("Usuário registrado:", response.data);
+    } catch (error) {
+      console.error(
+        "Erro ao registrar o usuário:",
+        error.response?.data || error
+      );
     }
+  };
 
+  if (userType === "representative") {
+    return (
+      <div className="form-container">
+        <div className="logo-container">
+          <img src="../../../../public/logo_branca.svg" alt="logo" />
+        </div>
+        <h1 className="title-page" style={{ marginTop: "0px" }}>
+          CADASTRO
+        </h1>
+        <div className="form-header">
+          <p>Representante Institucional</p>
+          <button className="change-user">
+            <Link to={"/cadastro"} className="ajust-link">
+              Trocar
+            </Link>
+          </button>
+        </div>
+        <form className="form" onSubmit={handleSubmit}>
+          <label htmlFor="name">
+            Nome completo <i>Obrigatório</i>
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            required
+            value={formData.name}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="email">
+            Email corporativo <i>Obrigatório</i>
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            value={formData.email}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="institution">
+            Instituição <i>Obrigatório</i>
+          </label>
+          <input
+            type="text"
+            id="institution"
+            name="institution"
+            required
+            value={formData.clinicalRole}
+            onChange={handleChange}
+          />
+
+          <div className="phone-number">
+            <label htmlFor="phone">
+              N° para contato <i>Obrigatório</i>
+            </label>
+            <InputMask
+              mask="(99)99999-9999"
+              required
+              value={formData.phone}
+              onChange={(e) =>
+                setFormData({ ...formData, phone: e.target.value })
+              }
+            >
+              {(inputProps) => (
+                <input {...inputProps} type="tel" id="phone" name="phone" />
+              )}
+            </InputMask>
+          </div>
+
+          <label htmlFor="clinicalRole">
+            Cargo/função <i>Obrigatório</i>
+          </label>
+          <input
+            type="text"
+            id="clinicalRole"
+            name="clinicalRole"
+            required
+            value={formData.clinicalRole}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="experience">
+            Experiências/qualificações <i>Obrigatório</i>
+          </label>
+          <textarea
+            id="experience"
+            name="experience"
+            rows={4}
+            value={formData.experience}
+            onChange={handleChange}
+          />
+
+          <div className="ajust-section">
+            <div>
+              <label htmlFor="password">
+                Criar senha <i>Obrigatório</i>
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                name="password"
+                required
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </div>
+            <button type="button" onClick={togglePasswordVisibility}>
+              Ver
+            </button>
+          </div>
+
+          <button type="submit" className="submit">
+            FAZER CADASTRO
+          </button>
+        </form>
+      </div>
+    );
+  } else {
+    return <div>AINDA EM PRODUÇÃO!!</div>;
+  }
 }
