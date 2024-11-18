68a4fde160d797c94d5e85bef032cb177ac93382
Ariano <arianosouzapro@gmail.com>
2024-11-17 21:32:45 -0300

    Telas do representante finalizadas

--- frontend/vite/src/pages/Representante/listpac.tsx
+++ frontend/vite/src/pages/Representante/listpac.tsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import Navbar from "../../components/Navbar";
+import { useSearchParams, Link } from "react-router-dom";
 import pacTest from './listpac.json'
+import studie from "./sigleStudieTest.json"
+
 
 export default function ListaPac(){
+    
+    const [seachParams] = useSearchParams()
+    const id = seachParams.get('id')
 
+    interface studieInfo {
+        title: String;
+        code: Number;
+    }
+    
     interface MedicalHistory{
         id: number;
         conditions: string[];
@@ -22,9 +33,16 @@ export default function ListaPac(){
 
     const pacInfoToAprove:Pacient[] = pacTest
     const pacInfoAproved:Pacient[] = pacTest
+    const infoPesq:studieInfo = {
+        title: studie.title,
+        code: studie.code
+    }
 
     const [selectedParticipantToAprove, setSelectedParticipantToAprove] = useState<Pacient>()
     const [selectedParticipantAproved, setSelectedParticipantAproved] = useState<Pacient>()
+    const [toAproveReproval, setToAproveReproval] = useState<Boolean>(false)
+    const [aprovedReproval, setAprovedReproval] = useState<Boolean>(false)
+    
     
 
     
@@ -33,14 +51,14 @@ export default function ListaPac(){
        <>
             <Navbar/>
             <div className="container-page">
-                <h1>PARTICIPANTES</h1>
-                <h2>Mecanismos Moleculares e Genéticos no Alzheimer: Novas Terapias - #0058</h2>
+                <h1 className="title-page">PARTICIPANTES</h1>
+                <h2 className="subtitle-pac">{infoPesq.title+ " - #" + infoPesq.code}</h2>
                 
-                <div className="section">
+                <div className="section-pac">
                     <h3>SOLICITAÇÕES</h3>
                     <div className="participant-list">
                     {pacInfoToAprove.map((pac) => (
-                        <button key={pac.code} onClick={()=>setSelectedParticipantToAprove(pac)}>
+                        <button key={pac.code} onClick={()=>{setSelectedParticipantToAprove(pac);setToAproveReproval(false)}}>
                         {pac.name}
                         </button>
                     ))}
@@ -48,21 +66,32 @@ export default function ListaPac(){
                 </div>
 
                 { selectedParticipantToAprove ? 
-                    <div>
+                    <div className="edit-patient">
                         <h2>{selectedParticipantToAprove?.name}</h2>
                         <p><strong>Código: </strong>{selectedParticipantToAprove?.code}</p>
                         <p><strong>Idade: </strong>{new Date().getFullYear() - new Date(selectedParticipantToAprove?.birth).getFullYear()}  </p>
                         <p><strong>Gênero: </strong> {selectedParticipantToAprove?.gender}</p>
-                        <div>
+                        <div >
                             <button>✔️</button>
-                            <button>❌</button>
+                            <button onClick={()=>setToAproveReproval(true)}>❌</button>
                         </div>
                     </div>
                     :
                     <></>
                 }
+                {
+                    toAproveReproval == true ? 
+                    <div className="reproval-motive">
+                        <label htmlFor="motive">Por qual motivo o paciente não foi aceito na pesquisa?</label>
+                        <textarea name="motive" id=""></textarea>
+                        <button>FINALIZAR</button>
+                        <button onClick={()=>setToAproveReproval(false)}>x</button>
+                    </div>
+                    :
+                    <></>
+                }
 
-            <div className="section">
+            <div className="section-pac">
                     <h3>APROVADOS</h3>
                     <div className="participant-list">
                     {pacInfoAproved.map((pac) => (
@@ -74,15 +103,25 @@ export default function ListaPac(){
                 </div>
 
                 { selectedParticipantAproved ? 
-                    <div>
+                    <div className="edit-patient">
                         <h2>{selectedParticipantAproved?.name}</h2>
                         <p><strong>Código: </strong>{selectedParticipantAproved?.code}</p>
                         <p><strong>Idade: </strong>{new Date().getFullYear() - new Date(selectedParticipantAproved?.birth).getFullYear()}  </p>
                         <p><strong>Gênero: </strong> {selectedParticipantAproved?.gender}</p>
                         <div>
-                            <button>✔️</button>
-                            <button>❌</button>
+                            <button onClick={()=>setAprovedReproval(true)}>❌</button>
                         </div>
+                    </div>
+                    :
+                    <></>
+                }
+                {
+                    aprovedReproval == true ? 
+                    <div className="reproval-motive">
+                        <label htmlFor="motive">Por que o paciente está sendo desligado da pesquisa?</label>
+                        <textarea name="motive" id=""></textarea>
+                        <button>FINALIZAR</button>
+                        <button onClick={()=>setAprovedReproval(false)}>x</button>
                     </div>
                     :
                     <></>
--- frontend/vite/src/pages/Representante/notificacoes.tsx
+++ frontend/vite/src/pages/Representante/notificacoes.tsx
@@ -1,15 +1,66 @@
 
+import { useState } from "react";
 import Navbar from "../../components/Navbar";
+import infoPesq from "../Representante/pesqPacientTest.json"
 
 
 export default function RepNotificacoes(){
+
+        const [cod,setCod] = useState(0)
+        const [files, setFiles] = useState<File[]>()
+
+        const haddleFiles = (e: React.ChangeEvent<HTMLInputElement>)=>{
+            const theFiles = e.target.files;
+            if (theFiles) {
+            // Convertendo o FileList para um array de File
+            const fileArray = Array.from(theFiles);
+
+            // Atualizando o estado com os arquivos selecionados
+            setFiles((prevFiles) => prevFiles ? [...prevFiles, ...fileArray]: [...fileArray]);
+            }
+        }
+
     return(
         <>
         <Navbar/>
                 <div className='container-page'> 
-                    <div>
-                        Notificações
+                    <h1 className="title-page">NOTIFICAÇÕES</h1>
+                    <div className="card-border">
+                        <div className="container-notfic-all">
+                            <label htmlFor="titulo">TÍTULO DA NOTIFICAÇÃO</label>
+                            <input type="text" name="titulo" />
+                            <label htmlFor="content">CONTEÚDO</label>
+                            <textarea name="content" id="content"></textarea>
+                            <label htmlFor="files" style={{alignSelf:"center"}}>ANEXOS</label>
+                            <div className="files-arrange" style={{alignSelf:"center"}}>
+                                <input type="file" name="files" multiple onChange={haddleFiles}/>
+                                +
+                            </div>
+                            <div style={{display:"flex"}}>
+                                {files?.map((f)=>(
+                                    <div className="files-custom">{f.name}</div>
+                                ))}
+                            </div>
+                            
+                            
+                            <div className="container-select">
+                                <select name="selectTypeUser" id="" onChange={(e)=>{setCod(Number(e.target.value))}}>
+                                    <option value="" disabled selected>Selecione a pesquisa</option>
+                                    {infoPesq.map((info,index)=>(
+                                        <option value={index}>{info.code}</option>
+                                    ))}
+                                </select>
+                                <select name="selectWho" id="" >
+                                <option value="" disabled selected>Selecione o paciente</option>
+                                    {infoPesq[cod].patients.map((info)=>(
+                                        <option value={info.id}>{info.name}</option>
+                                    ))}
+                                </select>
+                            </div>
+                            
+                        </div>
                     </div>
+                    <button className="button-send-notification">ENVIAR NOTIFICAÇÃO</button>
                 </div>
         </>
     )
