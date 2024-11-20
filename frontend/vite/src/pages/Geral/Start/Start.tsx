f5c90f0cccb54d562e8ae61e0e79fd320fed217b
Estephani Germana <129541178+estephgermana@users.noreply.github.com>
2024-11-20 19:08:46 -0300

    edição tela inicial

--- frontend/vite/src/pages/Geral/Start/Start.tsx
+++ frontend/vite/src/pages/Geral/Start/Start.tsx
@@ -1,21 +1,47 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import './Start.css';
 
+export default function Start() {
+  return (
+    <div className="container-all-start">
+      <div className="header-container">
+        <img
+          src="../../../../public/logo_branca.svg"
+          alt="logo"
+          className="logo-start"
+        />
+        <Link to="/login" className="link-login">
+          Login
+        </Link>
+      </div>
 
-export default function Start(){
-
-    return(
-        <div className="container-all-start">
-            <div className="header-container">
-                <img src="../../../../public/logo_branca.svg" alt="logo" className="logo-start"/>
-                <Link to='/login' className="link-login">Login</Link>
-            </div>
-                <img src="../../../../public/TRANSPAREÊNCIA E M PRIMEIRO LUGAR.jpg" alt="" className="front-page-image" />
-            <div className="rodape">
-                <img src="../../../../public/logo_branca.svg" alt="logo" className="logo-start"/>
-                <p>Inovamed - Rope Health</p>
-                <p>Todos os direitos reservados</p>
-            </div>
-            
+      <div className="front-page">
+        <div className="text-circle-container">
+          <div>
+            <h1>TRANSPARÊNCIA EM 1º LUGAR</h1>
+            <p>
+            A Inovamed é reconhecida por sua transparência na divulgação de pesquisas científicas, proporcionando acesso aberto a dados e fontes, o que permite a verificação rigorosa dos resultados. O site adota práticas de revisão por pares criteriosas e públicas, garantindo uma ciência aberta e acessível.
+            </p>
+          </div>
         </div>
-    )
+        <div className="doctor-image-container">
+          <img
+            src="../../../../public/medico.png"
+            alt="Médico"
+            className="doctor-image"
+          />
+        </div>
+      </div>
+
+      <div className="rodape">
+        <img
+          src="../../../../public/logo_branca.svg"
+          alt="logo"
+          className="logo-start"
+        />
+        <p>Inovamed - Rope Health</p>
+        <p>Todos os direitos reservados</p>
+      </div>
+    </div>
+  );
 }
