51ece3f6ae2e7fc3677a8de1b67b666e891acb48
AyrtonF <ayrtonleonardo14@gmail.com>
2024-11-25 10:14:39 -0300

    adicionando link do backend

--- frontend/vite/src/config/axiosConfig.ts
+++ frontend/vite/src/config/axiosConfig.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
+    baseURL: import.meta.env.VITE_API_BASE_URL || "https://inovamed-latest.onrender.com",
     //timeout: 10000, // Tempo limite opcional
    
     headers: {
