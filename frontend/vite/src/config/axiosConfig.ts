a369ac5944daad049122e81242e0f0a46102407f
andrefilipe1310 <andrefilipef1310@gmail.com>
2024-11-26 17:28:18 -0300

    update

--- frontend/vite/src/config/axiosConfig.ts
+++ frontend/vite/src/config/axiosConfig.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "https://inovamed-latest.onrender.com",
+    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
     headers: {
         "Content-Type": "application/json",
     },
