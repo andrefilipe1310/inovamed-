54c0bfed2bf70b1b8af137a133838b61a41c03c0
andrefilipe1310 <andrefilipef1310@gmail.com>
2024-11-25 22:56:47 -0300

    adicionando credenciais

--- frontend/vite/src/config/axiosConfig.ts
+++ frontend/vite/src/config/axiosConfig.ts
@@ -7,6 +7,7 @@ const api = axios.create({
     headers: {
         "Content-Type": "application/json",
     },
+    withCredentials: true
 });
 
 // Adicionar o token dinamicamente a cada requisição
--- frontend/vite/src/config/axiosUnauthorizedConfig.ts
+++ frontend/vite/src/config/axiosUnauthorizedConfig.ts
@@ -7,6 +7,7 @@ const apiUnauthorized = axios.create({
     headers: {
         "Content-Type": "application/json",
     },
+    withCredentials: true
 });
 
 export default apiUnauthorized;
